964cd0db81960bc25a946f2d173ca2bfdd84a5a6
FreeMan <zelonaire@gmail.com>
2023-02-16 23:01:14 +0000

    getting access to the DOM and storing them to a variable

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const randomNumer = document.getElementById("random-number");
+const inputNumer = document.getElementById("number-input");
+const checkBtn = document.getElementById("check-btn");
+const message = document.getElementById("message");
+const scoreRemaining = document.getElementById("score");
+const highScore = document.getElementById("high-score");
+
